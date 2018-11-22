861fc88cf3f811131b5f8228081f241e893b26ba
jv <ljw@live.jp>
2018-11-22 17:42:37 +0800

    refactor: 💡 deprecated router
    deprecated! widget-router removed
    
    BREAKING CHANGE: yes

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import routerView from './router-view'
-import routerLink from './router-link'
-
-export default {
-  install (Vue) {
-    Vue.component('WidgetRouterView', routerView)
-    Vue.component('WidgetRouterLink', routerLink)
-  }
-}
