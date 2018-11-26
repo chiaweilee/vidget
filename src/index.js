747907572b03450a75c624672c123ba8577ec9ee
jv <ljw@live.jp>
2018-11-26 09:43:34 +0800

    feat: 💡 optimize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { assert } from './utils/warn'
 
 const vidget = function (component = {}, Vue, options = {}) {
-  assert(Vue, 'require Vue')
-  assert(Vue && /^2\.\d+\.\d+$/.test(Vue.version), `require version 2+ of Vue, but ${Vue.version} was found.`)
+  assert(typeof Vue === 'function', 'require Vue')
+  assert(typeof Vue === 'function' && Vue.version && /^2\.\d+\.\d+$/.test(Vue.version), `require version 2.x of Vue, but ${Vue.version} was found.`)
   assert(typeof component === 'object' && !(component instanceof Array), 'component must be an object')
   assert(typeof options === 'object' && !(options instanceof Array), 'options must be an object')
-  assert(!(component.props && component.props.options), '\'props.options\' is reserved on root component, do not use it')
+  assert(!(component.props && component.props.options), '\'props.options\' is reserved on root component, avoid to use it')
 
   /* eslint-disable no-new */
   new Vue(Object.assign({
