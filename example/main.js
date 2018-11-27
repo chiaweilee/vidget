30eae342ac2de367f95ba5a870730c81581d37d1
jv <ljw@live.jp>
2018-11-27 09:35:23 +0800

    feat: 💡update

--- example/main.js
+++ example/main.js
@@ -4,16 +4,6 @@ import test from './components/test'
 
 buildWidget(test, Vue)
 
-// window.vidget('#widget', {
-//   appid: 12345667890
-// })
-
-setInterval(() => {
-  console.log('$init')
-  window.vidget.$init()
-  console.log('$destroy')
-  window.vidget.$destroy()
-  const widget = document.createElement('div')
-  widget.id = 'widget'
-  document.body.appendChild(widget)
-}, 1000)
+window.vidget('#widget', {
+  appid: 12345667890
+})
